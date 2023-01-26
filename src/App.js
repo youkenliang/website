9a6605f50d205f7ef85a671fd6bdeeaff223593d
youkenliang <youcanliang@berkeley.edu>
2023-01-26 12:01:08 -0800

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import "./index.css";
+import Home from './Home';
+import About from './About';
+import Projects from './Projects';
+import Contact from './Contact';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/projects" element={<Projects/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+      </Routes>
     </div>
   );
-}
+} 
 
 export default App;
---
+++ src/NavBar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Link, useMatch, useResolvedPath} from 'react-router-dom';
+
+function NavBar(){
+    return(
+        <nav className='nav'>
+            <ul>
+                <CustomLink to="/">Home</CustomLink>
+                <CustomLink to="/about">About</CustomLink>
+                <CustomLink to="/projects">Projects</CustomLink>
+                <CustomLink to="/contact">Contact</CustomLink>
+            </ul>
+        </nav>
+    )
+}
+
+function CustomLink({ to, children, ...props}){
+    const resolvePath = useResolvedPath(to)
+    const isActive = useMatch({path: resolvePath.pathname, end: true})
+    return (
+        <li className={isActive ? "active": ""}>
+            <Link to={to} {...props}>
+                {children}
+            </Link>
+        </li>
+
+    )
+}
+
+export default NavBar;
